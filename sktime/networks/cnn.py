cbab5282aeac9ec790a4e77e3c7997508b108c3d
paramsureliya <148708744+paramsureliya@users.noreply.github.com>
2026-04-23 14:19:16 +0200

    [ENH] Move CNNNetwork test skip to estimator tag

--- sktime/networks/cnn.py
+++ sktime/networks/cnn.py
@@ -54,6 +54,7 @@ class CNNNetwork(BaseDeepNetwork):
         # --------------
         "authors": ["hfawaz", "James-Large", "Withington", "TonyBagnall", "noxthot"],
         "python_dependencies": "tensorflow",
+        "tests:skip_by_name": ["test_inheritance"],  # see #8515, #3028
     }
 
     # TODO (release 0.41.0)
